a5972459511ef6fff0c2c3288a8d6421ea08dd2b
ki11errabbit <unlikelytitan@gmail.com>
2025-01-23 17:17:38 -0700

    added argparse

---
+++ src/calculator.py
@@ -0,0 +1,25 @@
+import argparse
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == 'main':
+    parser = argparse.ArgumentParser(
+            prog="Calculator",
+            description="A simple calculator to teach git"
+            )
+    parser.add_argument('expression')
+    args = parse_args()
