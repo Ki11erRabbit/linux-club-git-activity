59d1e99b9239b1eb02cc2efb7507c3f20b55f8cf
ki11errabbit <unlikelytitan@gmail.com>
2025-01-23 17:50:00 -0700

    added parsing

--- src/calculator.py
+++ src/calculator.py
@@ -1,17 +1,39 @@
 import argparse
 
+def parse_number(token, index):
+    return ((lambda: float(token)), index + 1)
+
+def parse_factor(tokens, index):
+    number, index = parse_number(tokens[index], index)
+    if index == len(tokens):
+        return number, index
+    operator = tokens[index]
+    expr, index = parse_addition(tokens, index + 1)
+    return ((lambda: print(number(), operator, expr())), index)
+
+def parse_addition(tokens, index):
+    number, index = parse_factor(tokens[index], index)
+    if index == len(tokens):
+        return number, index
+    operator = tokens[index]
+    expr, index = parse_factor(tokens, index + 1)
+    return ((lambda: print(number(), operator, expr())), index)
+
+def parse_tokens(tokens):
+    index = 0
+    function, index = parse_addition(tokens, index)
+    return function
 
 
-
-
-
-
-
+def parse(string):
+    tokens = string.split()
+    return parse_tokens(tokens)
 
 
 
 def main(expression):
     print(f'I am going to calculate: {expression}')
+    parse(expression)()
 
 
 
