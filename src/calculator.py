8954f1d2abe274a384ca5b277a766e71c4edd32d
ki11errabbit <unlikelytitan@gmail.com>
2025-01-23 17:22:16 -0700

    added main function

--- src/calculator.py
+++ src/calculator.py
@@ -10,16 +10,19 @@ import argparse
 
 
 
+def main(expression):
+    print(f'I am going to calculate: {expression}')
 
 
 
 
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
             prog="Calculator",
             description="A simple calculator to teach git"
             )
     parser.add_argument('expression')
-    args = parse_args()
+    args = parser.parse_args()
+    main(args.expression)
